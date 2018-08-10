2601b1d6b8c138ac2de329441013de38eb12c777
andreiHi <andreih1981@gmail.com>
2018-08-10 21:53:45 +0300

    initial commit

---
+++ src/main/java/com/example/sweater/Application.java
@@ -0,0 +1,20 @@
+package com.example.sweater;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Hincu Andrei (andreih1981@gmail.com)on 10.08.2018.
+ * @version $Id$.
+ * @since 0.1.
+ */
+@SpringBootApplication
+public class Application {
+    private static final Logger LOG = LogManager.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
